62a826ff3b35ac29b37df35e89ae6d1ee329e372
SinclairDee <dixinkai@outlook.com>
2017-08-31 16:33:04 +0000

    modified

--- 7月在线/四月份课程/零子数组.cc
+++ 7月在线/四月份课程/零子数组.cc
@@ -8,5 +8,19 @@
  * 对sum[0..N-1]排序，然后计算sum相邻元素的差，最小值记为min1
  * min1: 在A中任意取两个集合，各自元素的和求差的最小值。
  * 因为sum[-1] = 0,sum[0..N-1]的绝对值记为min2。(考虑负数和最最小值的影响)
- * 
- * 
+ * min2:A的前k个元素的和的绝对值的最小值
+ * 比较min1和min2的更小值，即为所求。
+ * status: haven't done
+ * **************************************/
+#include<iostream>
+using namespace std;
+
+int main(){
+int A[1024];
+int sum[1024];
+int N;
+for(int i = 0; i < N; i++){
+    for(int)
+}
+}
+ 
