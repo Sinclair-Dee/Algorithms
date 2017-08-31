18bf14f8f7a201ac91a2dd29321969b467c60621
SinclairDee <dixinkai@outlook.com>
2017-08-31 15:19:25 +0000

    修改

--- DP/PaPartition2.cc
+++ DP/PaPartition2.cc
@@ -2,6 +2,7 @@
  * leetcode
  * 132. Palindrome Partitioning II
  * 2017/8/23
+ * status: I will sove this problem
  * 参考：
  * http://blog.csdn.net/jin_kwok/article/details/51423222
  * http://www.cnblogs.com/ganganloveu/p/3982561.html
