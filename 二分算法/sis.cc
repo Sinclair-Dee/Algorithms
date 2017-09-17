0ed836f8d75048d7f59d9ad153f4a65447c44e1c
SinclairDee <dixinkai@outlook.com>
2017-09-17 06:22:54 +0000

    add sis

--- 二分算法/sis.cc
+++ 二分算法/sis.cc
@@ -10,6 +10,18 @@
   # define MAXNUM  1000
   
   //算法思想，二分查找
+  //找到这个最大值的上界和下界限
+  //上界：max = num[0]+num[1]+num[2]+...num[n-1]
+  //下界：min = max/m
+  //取上下界的中间值mid,然后开始在数组中取序列，刚好大于等于mid的时候开始再取下一个序列。
+  //数最后取得序列的个数k
+  //若k>m:说明mid取小了，再从mid到max之间二分选取。
+  //若k<m;说明mid取大了，再从min到mid之间取值。
+  //注意k=m 的时候实际上还可以把mid往大处取值，与k>m情况一样处理。
+  //最后的结果是min 和 max 只相差一个数，mid 向下取整取的min.
+  //思考：其实最后不一定取min和max哪个是最好的结果，需要筛选，这个过程不是算法的核心内容，略过了。
+  //上边给出的测试用例是取得min,而且恰好min比较合适。
+  //binarysearch：用二分法寻找合适的最大值
   int binarysearch(int num[], int n, int m,int min, int max){
   	int sum = 0;
   	int k = 0;
